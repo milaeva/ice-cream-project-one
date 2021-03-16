5ffa545692d498b5730259e60356b097cf1dc44c
Vitaliia <milayevavita27@gmail.com>
2021-03-17 01:10:50 +0200

    add

---
+++ src/js/burger.js
@@ -0,0 +1,44 @@
+// Бургер меню
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.menu__button,.buy-now');
+  let links = menu.find('.menu__link');
+  let overlay = menu.find('.menu__overlay');
+
+  button.on('click', e => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass('menu_active');
+    if (menu.hasClass('menu_active')) {
+      $('body').css('overflow', 'hidden');
+    } else {
+      $('body').css('overflow', 'visible');
+    }
+  }
+}
+
+burgerMenu('.menu');
+
+// Скрипт кнопки Вверх
+
+// var btn = $('#top__button');
+
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > 500) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('header').animate({ scrollTop: 0 }, '300');
+// });
---
+++ src/js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtnTab: document.querySelector('[data-modal-open-tab]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnTab.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
